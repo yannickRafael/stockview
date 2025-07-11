2dc8aec180dcde0068b5b1852d6835d7af93e7af
yannickRafael <yannickrafael286@gmail.com>
2025-07-11 18:25:23 +0200

    Implement main execution flow for DocumentProcessor with auto-processing and saving functionality

--- app.py
+++ app.py
@@ -2,3 +2,9 @@ from document_processor import DocumentProcessor
 from config import Config as c
 
 
+if __name__=='__main__':
+    dp = DocumentProcessor(auto_process=True, file_path=c.FILE_PATH)
+    dp.save_blocks("blocos.xlsx")
+    dp.save_blocks("blocos.json")
+
+    dp.save_tables(output_dir="tabelas", format="xlsx")
